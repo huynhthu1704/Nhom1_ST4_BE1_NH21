46bc44ec06df2a657e7923726cdf6d4ba6ea3e84
Huynh Thi Ngoc Thu <huynhthingocthu1704@gmail.com>
2021-11-03 11:50:06 +0700

    Change email, address, manufactures, types

---
+++ .history/config_20211027075639.php
@@ -0,0 +1,7 @@
+<?php
+define ('DB_NAME', 'nhom1');
+define ('DB_USER', 'root');
+define ('DB_PASSWORD', 'root');
+define ('DB_HOST', 'localhost');
+define ('PORT', );
+define ('DB_CHARSET', 'utf8');
