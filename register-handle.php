3232cbc5f366e27ebd5534bbb3a2f938ff3e1924
DucTri <nguyenthanhductri.tdc2020@gmail.com>
2021-11-30 20:40:42 +0700

    update register

--- register-handle.php
+++ register-handle.php
@@ -2,26 +2,38 @@
 require "config.php";
 require "models/db.php";
 require "models/product.php";
-require "models/protype.php"; 
+require "models/protype.php";
 require "models/customer.php"; ?>
 <?php
-    if (isset($_POST['submit'])) {
-        $first = isset($_POST['first'])? $_POST['first'] : "";
-        $last = isset($_POST['last']) ? $_POST['last']: "";
-        $phone =isset( $_POST['phone']) ?  $_POST['phone']: "";
-        $address = isset( $_POST['address']) ?  $_POST['address']: "";
-        $email = isset( $_POST['email']) ?  $_POST['email']: "";
-        $gender = isset( $_POST['gender']) ?  $_POST['gender']: "";
-        $username=isset($_POST['user'])? $_POST['user']:"";
-        $pass=isset($_POST['password']) ? $_POST['password']:"";
-        $birtday= isset($_POST['birthday']) ? $_POST['birthday']:"";
-        $retypepassword=isset($_POST['retype-password'])? $_POST['retype-password']: "";
-        if ($retypepassword!=$pass) {
-           header("Location: register.php");
-        }else{
-            $customer =  new Customer();
-            
-            $addNewCustomer = $customer->addNewCustomer($first,$last,$phone,$address,$email,$gender,$birtday);
+if (isset($_POST['submit'])) {
+    $customer =  new Customer();
+    $first =  $_POST['first'];
+    $last = $_POST['last'];
+    $phone = $_POST['phone'];
+    $address =  $_POST['address'];
+    $email =  $_POST['email'];
+    $gender = $_POST['gender'];
+    $username = $_POST['user'];
+    $pass = $_POST['password'];
+    $retypepassword = $_POST['retype-password'];
+    $birtday = $_POST['birthday'];
+    $city = $_POST['city'];
+    $zipcode = $_POST['zipcode'];
+    $checkname = $customer->checkUser($username);
+    $check;
+    foreach ($checkname as $values) {
+        $check = $values['username'];
+    }
+    if ($retypepassword != $pass) {
+        echo "<script> alert('Tài khoản đã tồn tại'); </script>";
+        header("Location: register.php");
+    } else {
+        if ($username == $check) {
+            echo "<script> alert('Registered name was used'); </script>";
+            header("Location: register.php");
+        } else {
+            $addNewCustomer = $customer->addNewCustomer($first,$last,$phone,$address,$city,$zipcode,$email,$gender,$birtday,$username,$pass);
         }
-      }
+    }
+}
 ?>
